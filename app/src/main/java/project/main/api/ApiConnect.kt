75f1696f494dda19edf08903edb8bd8cbc56dc23
timmymike <t19880916@yahoo.com.tw>
2021-08-15 22:28:50 +0800

    1.刪除不必要的 implementation 2.FloatingActionButton水波紋顏色修改為白色

--- app/src/main/java/project/main/api/ApiConnect.kt
+++ app/src/main/java/project/main/api/ApiConnect.kt
@@ -3,13 +3,11 @@ package project.main.api
 import android.content.Context
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ApiConnect {
-
 
     private var apiService: ApiService? = null
 
